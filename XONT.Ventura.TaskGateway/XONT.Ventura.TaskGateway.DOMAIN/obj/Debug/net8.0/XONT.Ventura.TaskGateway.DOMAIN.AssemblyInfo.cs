2e718573a232137c5ff831d48ab50b4dd6b87637
lahiruXONT <lahiru.d@xontworld.com>
2025-08-14 08:35:35 +0530

    edit launch

--- XONT.Ventura.TaskGateway/XONT.Ventura.TaskGateway.DOMAIN/obj/Debug/net8.0/XONT.Ventura.TaskGateway.DOMAIN.AssemblyInfo.cs
+++ XONT.Ventura.TaskGateway/XONT.Ventura.TaskGateway.DOMAIN/obj/Debug/net8.0/XONT.Ventura.TaskGateway.DOMAIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XONT.Ventura.TaskGateway.DOMAIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0f838fed299735e133ac5989f69558d54af65c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46dbda97fa4e0164a630f3a70d980fab92a0b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("XONT.Ventura.TaskGateway.DOMAIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XONT.Ventura.TaskGateway.DOMAIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- XONT.Ventura.TaskGateway/XONT.Ventura.TaskGateway/obj/Debug/net8.0/XONT.Ventura.TaskGateway.AssemblyInfo.cs
+++ XONT.Ventura.TaskGateway/XONT.Ventura.TaskGateway/obj/Debug/net8.0/XONT.Ventura.TaskGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XONT.Ventura.TaskGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0f838fed299735e133ac5989f69558d54af65c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46dbda97fa4e0164a630f3a70d980fab92a0b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("XONT.Ventura.TaskGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XONT.Ventura.TaskGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
